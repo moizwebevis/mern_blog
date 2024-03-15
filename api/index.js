afdc2332f5d871ab8d183618de4feb3f5d16d08c
Moiz <moiz@webevis.com>
2024-03-15 21:03:15 +0500

    fixed

--- api/index.js
+++ api/index.js
@@ -31,13 +31,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
 
-// app.use(express.static(path.join(__dirname, 'client/dist')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-// });
-
-
+// For all other routes, serve the 'index.html' file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+});
 
 app.get('/', (req, res) => {
     res.send("Hello from Node API server!");
