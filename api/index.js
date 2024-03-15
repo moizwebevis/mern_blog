36e7f66e037beff938b4eccc1a2fbc25e005c1a4
Moiz <moiz@webevis.com>
2024-03-15 19:50:26 +0500

    changes issue

--- api/index.js
+++ api/index.js
@@ -1,21 +1,21 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js';
-import postRoutes from './routes/post.route.js';
-import commentRoutes from './routes/comment.route.js';
+// import userRoutes from './routes/user.route.js';
+// import authRoutes from './routes/auth.route.js';
+// import postRoutes from './routes/post.route.js';
+// import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
 
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO,{ useNewUrlParser: true })
+mongoose.connect(process.env.MONGO)
 .then(() => console.log('MongoDB is Connected!'))
 .catch(err => console.log(err));
 
-console.log(process?.env.MONGO, 'env')
+
 const __dirname = path.resolve();
 
 const app = express()
@@ -26,10 +26,13 @@ app.listen(3000, () => {
     console.log("Server is running on port 3000");   
 });
 
-app.use("/api/user", userRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/post", postRoutes);
-app.use("/api/comment", commentRoutes);
+app.get('/test', (req, res) => {
+    res.send("Hello testing");
+});
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/post", postRoutes);
+// app.use("/api/comment", commentRoutes);
 
 app.use(express.static(path.join(__dirname, 'client/dist')));
 
